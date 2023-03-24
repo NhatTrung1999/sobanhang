cc1b8bd48aa662cf326bb01a7b5c4f55406106c8
Nguyen Nhat Trung <trungnguyen15051999@gmail.com>
2023-03-24 23:11:31 +0700

    update1

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -6,116 +6,184 @@ import playstore from "../../img/playstore.png";
 import StarIcon from "@mui/icons-material/Star";
 
 function Banner() {
-  return (
-    <Box>
-      <Box
-        display="flex"
-        justifyContent="center"
-        paddingLeft="40px"
-        paddingRight="40px"
-      >
-        <Box position="relative">
-          <img src={BannerLogo} style={{ maxWidth: "100%", height: "auto" }} />
-          <Container>
-            <Grid container>
-              <Grid item lg={6}>
-                <Box position="absolute" top="22%" maxWidth="29%">
-                  <Box
-                    fontSize="60px"
-                    lineHeight="65px"
-                    color="#fff"
-                    marginBottom="30px"
-                  >
-                    Bán hàng dễ, tiền về nhanh{" "}
-                  </Box>
-                  <Box
-                    fontSize="18px"
-                    lineHeight="32px"
-                    color="#fff"
-                    marginBottom="42px"
-                  >
-                    Mở website bán hàng, quản lý toàn diện cửa hàng và thu tiền
-                    ngay trên điện thoại với Sổ Bán Hàng
-                  </Box>
-                  <Box>
-                    <Grid container spacing={0.5}>
-                      <Grid item>
-                        <img src={appstore} width="135" height="40" />
-                      </Grid>
-                      <Grid item>
-                        <img src={playstore} width="135" height="40" />
-                      </Grid>
+    return (
+        <Box>
+            <Box
+                display="flex"
+                justifyContent="center"
+                paddingLeft="40px"
+                paddingRight="40px"
+            >
+                <Box position="relative">
+                    <img
+                        src={BannerLogo}
+                        style={{ maxWidth: "100%", height: "auto" }}
+                    />
+                    <Container>
+                        <Grid container>
+                            <Grid item lg={6} sx={{ display: { xs: "none" } }}>
+                                <Box
+                                    position="absolute"
+                                    top="22%"
+                                    maxWidth="29%"
+                                >
+                                    <Box
+                                        fontSize="60px"
+                                        lineHeight="65px"
+                                        color="#fff"
+                                        marginBottom="30px"
+                                    >
+                                        Bán hàng dễ, tiền về nhanh{" "}
+                                    </Box>
+                                    <Box
+                                        fontSize="18px"
+                                        lineHeight="32px"
+                                        color="#fff"
+                                        marginBottom="42px"
+                                    >
+                                        Mở website bán hàng, quản lý toàn diện
+                                        cửa hàng và thu tiền ngay trên điện
+                                        thoại với Sổ Bán Hàng
+                                    </Box>
+                                    <Box>
+                                        <Grid container spacing={0.5}>
+                                            <Grid item>
+                                                <img
+                                                    src={appstore}
+                                                    width="135"
+                                                    height="40"
+                                                />
+                                            </Grid>
+                                            <Grid item>
+                                                <img
+                                                    src={playstore}
+                                                    width="135"
+                                                    height="40"
+                                                />
+                                            </Grid>
+                                        </Grid>
+                                        <p
+                                            style={{
+                                                color: "#fff",
+                                                fontSize: "15px",
+                                            }}
+                                        >
+                                            Cực DỄ SỬ DỤNG và hoàn toàn MIỄN PHÍ{" "}
+                                        </p>
+                                    </Box>
+                                </Box>
+                            </Grid>
+                            <Grid item lg={6}>
+                                <Box
+                                    position="absolute"
+                                    top="-23px"
+                                    right="176px"
+                                >
+                                    <img
+                                        src={bannerHeader}
+                                        style={{
+                                            maxWidth: "100%",
+                                            height: "auto",
+                                        }}
+                                    />
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Box>
+            <Container>
+                <Box
+                    display="flex"
+                    justifyContent="space-evenly"
+                    paddingTop="90px"
+                    paddingBottom="65px"
+                >
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Box flex={1}>
+                                <Box
+                                    fontSize="36px"
+                                    fontWeight="700"
+                                    sx={{
+                                        fontSize: { xs: "26px" },
+                                        letterSpacing: { xs: "-1.2px" },
+                                    }}
+                                >
+                                    Được hơn 400,000+ chủ kinh <br />
+                                    doanh nhỏ tin dùng sau 1 năm ra mắt
+                                </Box>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Box
+                                flex="1"
+                                display="flex"
+                                justifyContent="space-around"
+                            >
+                                <Grid container>
+                                    <Grid item xs={12}>
+                                        <Box>
+                                            <Box
+                                                fontSize="50px"
+                                                fontWeight={700}
+                                                marginBottom="15px"
+                                            >
+                                                30,000+
+                                            </Box>
+                                            <Box
+                                                fontSize={17}
+                                                maxWidth="70%"
+                                                fontWeight="100"
+                                                sx={{ maxWidth: { xs: "39%" } }}
+                                            >
+                                                Cửa hàng mới mỗi tháng{" "}
+                                            </Box>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Box>
+                                            <Box
+                                                fontSize={50}
+                                                fontWeight={700}
+                                                marginBottom="15px"
+                                            >
+                                                4.8
+                                            </Box>
+                                            <Box
+                                                fontSize={17}
+                                                marginBottom="5px"
+                                            >
+                                                <i style={{ color: "#F5A623" }}>
+                                                    <StarIcon fontSize="small" />
+                                                </i>
+                                                <i style={{ color: "#F5A623" }}>
+                                                    <StarIcon fontSize="small" />
+                                                </i>
+                                                <i style={{ color: "#F5A623" }}>
+                                                    <StarIcon fontSize="small" />
+                                                </i>
+                                                <i style={{ color: "#F5A623" }}>
+                                                    <StarIcon fontSize="small" />
+                                                </i>
+                                                <i style={{ color: "#F5A623" }}>
+                                                    <StarIcon fontSize="small" />
+                                                </i>
+                                            </Box>
+                                            <Box fontSize={17} fontWeight="100">
+                                                12,380 đánh giá
+                                            </Box>
+                                        </Box>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Grid>
                     </Grid>
-                    <p style={{ color: "#fff", fontSize: "15px" }}>
-                      Cực DỄ SỬ DỤNG và hoàn toàn MIỄN PHÍ{" "}
-                    </p>
-                  </Box>
                 </Box>
-              </Grid>
-              <Grid item lg={6}>
-                <Box position="absolute" top="-23px" right="176px">
-                  <img
-                    src={bannerHeader}
-                    style={{ maxWidth: "100%", height: "auto" }}
-                  />
-                </Box>
-              </Grid>
-            </Grid>
-          </Container>
+            </Container>
         </Box>
-      </Box>
-      <Container>
-        <Box
-          display="flex"
-          justifyContent="space-evenly"
-          paddingTop="90px"
-          paddingBottom="65px"
-        >
-          <Box flex={1}>
-            <Box fontSize="36px" fontWeight="100">
-              Được hơn 400,000+ chủ kinh <br />
-              doanh nhỏ tin dùng sau 1 năm ra mắt
-            </Box>
-          </Box>
-          <Box flex="1" display="flex" justifyContent="space-around">
-            <Box>
-              <Box fontSize="50px" fontWeight={100} marginBottom="15px">
-                30,000+
-              </Box>
-              <Box fontSize={17} maxWidth="70%" fontWeight="100">
-                Cửa hàng mới mỗi tháng{" "}
-              </Box>
-            </Box>
-            <Box>
-              <Box fontSize={50} fontWeight={100} marginBottom="15px">
-                4.8
-              </Box>
-              <Box fontSize={17} marginBottom="5px">
-                <i style={{ color: "#F5A623" }}>
-                  <StarIcon fontSize="small" />
-                </i>
-                <i style={{ color: "#F5A623" }}>
-                  <StarIcon fontSize="small" />
-                </i>
-                <i style={{ color: "#F5A623" }}>
-                  <StarIcon fontSize="small" />
-                </i>
-                <i style={{ color: "#F5A623" }}>
-                  <StarIcon fontSize="small" />
-                </i>
-                <i style={{ color: "#F5A623" }}>
-                  <StarIcon fontSize="small" />
-                </i>
-              </Box>
-              <Box fontSize={17} fontWeight="100">
-                12,380 đánh giá
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
-  );
+    );
 }
 
 export default Banner;
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -2,77 +2,101 @@ import "./Navbar.css";
 import Logo from "../../img/cropped-cropped-logo-website.png";
 import { Box, Grid, Link, List, ListItem, Divider } from "@mui/material";
 import { Container } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
-  return (
-    <Container>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        paddingTop="15px"
-        paddingBottom="15px"
-      >
-        <Box>
-          <img src={Logo} width="120" height="29.813" />
-        </Box>
-        <Box display="flex" alignItems="center">
-          <Box>
-            <List>
-              <Grid container spacing={2}>
-                <Grid item>
-                  <ListItem className="nav-link" sx={{ fontWeight: "100" }}>
-                    <Link color="black" underline="none">
-                      Chợ sổ
-                    </Link>
-                  </ListItem>
-                </Grid>
-                <Grid item>
-                  <ListItem className="nav-link" sx={{ fontWeight: "100" }}>
-                    <Link color="black" underline="none">
-                      Cộng đồng
-                    </Link>
-                  </ListItem>
-                </Grid>
-                <Grid item>
-                  <ListItem className="nav-link" sx={{ fontWeight: "100" }}>
-                    <Link color="black" underline="none">
-                      Blog
-                    </Link>
-                  </ListItem>
-                </Grid>
-                <Grid item>
-                  <ListItem className="nav-link" sx={{ fontWeight: "100" }}>
-                    <Link color="black" underline="none">
-                      Hỗ trợ
-                    </Link>
-                  </ListItem>
-                </Grid>
-                <Grid item>
-                  <ListItem className="nav-link" sx={{ fontWeight: "100" }}>
-                    <Link color="black" underline="none">
-                      Bảng giá
-                    </Link>
-                  </ListItem>
-                </Grid>
-              </Grid>
-            </List>
-          </Box>
-          <Divider orientation="vertical" variant="middle" flexItem />
-          <Box className="login">
-            <Link underline="none" color="#0E8740" fontWeight={100}>
-              Đăng nhập
-            </Link>
-          </Box>
-          <Box className="register">
-            <Link underline="none" color="#fff" fontWeight={100}>
-              Đăng ký
-            </Link>
-          </Box>
-        </Box>
-      </Box>
-    </Container>
-  );
+    return (
+        <Container>
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                paddingTop="15px"
+                paddingBottom="15px"
+            >
+                <Box>
+                    <img src={Logo} width="120" height="29.813" />
+                </Box>
+                <Box>
+                    <MenuIcon />
+                </Box>
+                <Box display="flex" alignItems="center">
+                    <Box>
+                        <List>
+                            <Grid container spacing={2}>
+                                <Grid item sx={{ display: { xs: "none" } }}>
+                                    <ListItem
+                                        className="nav-link"
+                                        sx={{ fontWeight: "100" }}
+                                    >
+                                        <Link color="black" underline="none">
+                                            Chợ sổ
+                                        </Link>
+                                    </ListItem>
+                                </Grid>
+                                <Grid item sx={{ display: { xs: "none" } }}>
+                                    <ListItem
+                                        className="nav-link"
+                                        sx={{ fontWeight: "100" }}
+                                    >
+                                        <Link color="black" underline="none">
+                                            Cộng đồng
+                                        </Link>
+                                    </ListItem>
+                                </Grid>
+                                <Grid item sx={{ display: { xs: "none" } }}>
+                                    <ListItem
+                                        className="nav-link"
+                                        sx={{ fontWeight: "100" }}
+                                    >
+                                        <Link color="black" underline="none">
+                                            Blog
+                                        </Link>
+                                    </ListItem>
+                                </Grid>
+                                <Grid item sx={{ display: { xs: "none" } }}>
+                                    <ListItem
+                                        className="nav-link"
+                                        sx={{ fontWeight: "100" }}
+                                    >
+                                        <Link color="black" underline="none">
+                                            Hỗ trợ
+                                        </Link>
+                                    </ListItem>
+                                </Grid>
+                                <Grid item sx={{ display: { xs: "none" } }}>
+                                    <ListItem
+                                        className="nav-link"
+                                        sx={{ fontWeight: "100" }}
+                                    >
+                                        <Link color="black" underline="none">
+                                            Bảng giá
+                                        </Link>
+                                    </ListItem>
+                                </Grid>
+                            </Grid>
+                        </List>
+                    </Box>
+                    <Divider
+                        orientation="vertical"
+                        variant="middle"
+                        flexItem
+                        sx={{ display: { xs: "none" } }}
+                    />
+                    <Box className="login" sx={{ display: { xs: "none" } }}>
+                        <Link underline="none" color="#0E8740" fontWeight={100}>
+                            Đăng nhập
+                        </Link>
+                    </Box>
+                    <Box className="register" sx={{ display: { xs: "none" } }}>
+                        <Link underline="none" color="#fff" fontWeight={100}>
+                            Đăng ký
+                        </Link>
+                    </Box>
+                </Box>
+            </Box>
+        </Container>
+    );
 }
 
 export default Navbar;
